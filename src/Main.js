b07a77c3c8c6e8b530bf02d73cd9652be224ecf7
superastorh <salem.m.s2030@gmail.com>
2021-02-07 16:34:45 +0300

    first commit

---
+++ src/Main.js
@@ -0,0 +1,35 @@
+const router = new Navigo("/");
+var options = {
+  valueNames: ["category", "zekr"],
+  // Since there are no elements in the list, this will be used as template.
+  item: `
+    <div class="card border-secondary mb-3">
+      <div class="card-body text-secondary">
+      <h5 class="card-title zekr"></h5>
+      <p class="card-text category"></p>
+    </div>
+    `
+};
+var userList = new List("azkar", options, azkar);
+let tap = $(".nav-item");
+
+tap.click(function () {
+  tap.removeClass("active");
+  this.classList.add("active");
+  ls(this.getAttribute("data-set"));
+});
+
+function ls(name) {
+  if (name == "all") {
+    router.navigate("");
+    userList.filter(() => true);
+  } else {
+    router.navigate("?type=" + type);
+    userList.filter((e) => e["_values"].category == name);
+  }
+}
+
+let paramsString = location.search;
+let type = new URLSearchParams(paramsString).get("type");
+
+$('.nav-item[data-set="' + type + '"]').click();
